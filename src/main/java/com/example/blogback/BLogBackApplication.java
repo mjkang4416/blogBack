e5dbe11bfe560449c7ae268e4e944ce7735c61e6
s0obang <tnqls40168@gmail.com>
2024-05-13 14:13:35 +0900

    first commit

---
+++ src/main/java/com/example/blogback/BLogBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BLogBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BLogBackApplication.class, args);
+	}
+
+}
