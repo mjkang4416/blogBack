06d5e14b84fc5bdfa8ac0543092a269e87104e9c
s0obang <tnqls40168@gmail.com>
2024-06-29 00:58:07 +0900

    1. Add swagger dependency , configuartion/2. mypage post api using nickname(with temprary user create api)

---
+++ src/main/java/com/example/blogback/config/SwaggerConfig.java
@@ -0,0 +1,2 @@
+package com.example.blogback.config;public class SwaggerConfig {
+}
---
+++ src/main/java/com/example/blogback/controller/accounts/UserController.java
@@ -0,0 +1,28 @@
+package com.example.blogback.controller.accounts;
+
+
+import com.example.blogback.Service.UserService;
+import com.example.blogback.dto.accounts.UserDTO;
+import com.example.blogback.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor//여기서 생성자 의존성 주입 되는거에여 안되는거 아님 @Autuwired는 없지만 final로 선언돼서 생성자에서 초기화되면서 자동 주입함
+public class UserController {
+    public final UserService userService;
+
+    @GetMapping("/mypage/{nickname}")
+    public UserEntity getUserMypage(@PathVariable String nickname) {
+        // 닉네임으로 사용자 정보를 조회하는거ㅣㅁ
+        return userService.getUserByNickname(nickname);
+    }
+    //유저 정보 조회할라면 유저가 필요해서 그거 만드는 용이에여 임시일 뿐
+    @PostMapping("/create")
+    public UserEntity createUser(@RequestBody UserDTO userDTO) {
+        return userService.createUser(userDTO);
+    }
+}
