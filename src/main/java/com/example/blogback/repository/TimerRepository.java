00715e50512aa31884f041f138163fb3e58d826c
seoyeonii <chaesy02@gmail.com>
2024-07-01 20:07:49 +0900

    리포지토리 내ㅛ용 없앰

--- src/main/java/com/example/blogback/repository/TimerRepository.java
+++ src/main/java/com/example/blogback/repository/TimerRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TimerRepository extends JpaRepository<TimerEntity, Long> {
-    TimerEntity findByUserId(Long userId);
-    TimerEntity findByBookId(Long bookId);
 }
