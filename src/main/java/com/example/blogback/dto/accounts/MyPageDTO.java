319ba370d43eb170e039e362883eb381e44bd2ef
s0obang <tnqls40168@gmail.com>
2024-07-07 15:20:44 +0900

    mypage

--- src/main/java/com/example/blogback/dto/accounts/MyPageDTO.java
+++ src/main/java/com/example/blogback/dto/accounts/MyPageDTO.java
@@ -1,26 +1,19 @@
 package com.example.blogback.dto.accounts;
 
-
 import com.example.blogback.dto.blog.BookLogDTO;
-import lombok.AllArgsConstructor;
+import com.example.blogback.entity.BookLogEntity;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-
-public class UserDTO {
+public class MyPageDTO {
     private Long userId;
     private String nickname;
-    private String password;
     private String email;
     private String img_url;
     private String intro;
-    private List<BookLogDTO> bookLogs;
-
+    private List<BookLogEntity> bookLogs; // BookLogs 포함
 }
