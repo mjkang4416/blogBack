0a45a2e0b83fbf11ba9c3f5341e8fd0fbff6ab2e
s0obang <tnqls40168@gmail.com>
2024-07-12 07:52:16 +0900

    친구 북로그, 친구 검색

--- src/main/java/com/example/blogback/controller/friends/FriendsController.java
+++ src/main/java/com/example/blogback/controller/friends/FriendsController.java
@@ -1,25 +1,40 @@
 package com.example.blogback.controller.friends;
 
 import com.example.blogback.Service.FriendsService;
+import com.example.blogback.Service.UserService;
 import com.example.blogback.dto.friends.FriendPageDTO;
+import com.example.blogback.entity.BookLogEntity;
 import com.example.blogback.entity.FriendsEntity;
+import com.example.blogback.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/friends")
 @RequiredArgsConstructor
 public class FriendsController {
     private final FriendsService friendsService;
+    private final UserService userService;
 
     @PostMapping("/add")
-    public FriendsEntity addFriend(@RequestParam String userNickname, @RequestParam String friendNickname) {
+    public FriendsEntity addFriend(@AuthenticationPrincipal UserEntity userEntity, @RequestParam String friendNickname) {
 
-        return friendsService.addFriend(userNickname, friendNickname);
+        return friendsService.addFriend(userEntity.getNickname(), friendNickname);
     }
     @GetMapping("/page")
     public FriendPageDTO getFriendPage(@RequestParam Long userId) {
         return friendsService.getFriendPage(userId);
+    }
+    @GetMapping("/bookLog")
+    public List<BookLogEntity> getFriendBookLogs(@AuthenticationPrincipal UserEntity userId) {
+        return friendsService.getFriendsBookLogs(userId);
+    }
+    @GetMapping("/search")
+    public UserEntity getFriendByUserId(Long userId) {
+        return userService.getUserByUserId(userId);
     }
 }
