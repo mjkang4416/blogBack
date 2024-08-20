c5b752f9b1fb4335abc282f82ee6ae02a2efcf37
seoyeonii <chaesy02@gmail.com>
2024-08-20 13:35:44 +0900

    [feat] 타이머 기능 추가

--- src/main/java/com/example/blogback/controller/timer/TimerController.java
+++ src/main/java/com/example/blogback/controller/timer/TimerController.java
@@ -7,9 +7,12 @@ import com.example.blogback.dto.timer.RecordingDTO;
 import com.example.blogback.dto.timer.TimerDTO;
 import com.example.blogback.entity.DateEntity;
 import com.example.blogback.entity.TimerEntity;
+import com.example.blogback.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -18,13 +21,17 @@ import java.util.concurrent.CompletableFuture;
 public class TimerController {
     private final TimerService timerService;
     // 기록된 시간 보여줌
+    @GetMapping("/recorded")
+    public List<TimerEntity> recordedTime(@AuthenticationPrincipal UserEntity userEntity) {
+        return timerService.getRecordedTime(userEntity);
+    }
 
     // 프론트에서 넘겨받은 시간 저장
     @PostMapping("/recording")
-    public void saveTimeAndDate(@RequestBody RecordingDTO recordingDTO) {
-        CompletableFuture<TimerEntity> recordTime = timerService.recordTimeAsync(recordingDTO.getTimerDTO());
-        CompletableFuture<DateEntity> recordDate = timerService.recordDateAsync(recordingDTO.getDateDTO());
+    public RecordingDTO saveTimeAndDate(@AuthenticationPrincipal UserEntity userEntity, @RequestBody RecordingDTO recordingDTO) {
+        CompletableFuture<TimerEntity> recordTime = timerService.recordTimeAsync(userEntity, recordingDTO.getTimerDTO());
+        CompletableFuture<DateEntity> recordDate = timerService.recordDateAsync(userEntity, recordingDTO.getDateDTO());
         CompletableFuture.allOf(recordTime, recordDate).join();
-
+        return recordingDTO;
     }
 }
--- src/main/java/com/example/blogback/repository/DateRepository.java
+++ src/main/java/com/example/blogback/repository/DateRepository.java
@@ -1,7 +1,18 @@
 package com.example.blogback.repository;
 
 import com.example.blogback.entity.DateEntity;
+import com.example.blogback.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.util.List;
 
 public interface DateRepository extends JpaRepository<DateEntity, Long> {
+    @Query("SELECT SUM(d.timeRecorded), DATE(d.createDate) AS day " +
+            "FROM DateEntity d " +
+            "WHERE d.userId = :userId " +
+            "GROUP BY DATE(d.createDate) " +
+            "ORDER BY DATE(d.createDate) ASC")
+    List<Object[]> getSumDateRecord(@AuthenticationPrincipal UserEntity userId);
 }
